4d9857d47404f9add9b268a1ef1ef144dc203431
zyzisyz <zyzisyz>
2022-06-03 22:07:23 +0800

    update

--- wespeaker/models/mfa_conformer.py
+++ wespeaker/models/mfa_conformer.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# Author: Yang Zhang, Zhiqiang Lv
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -8,17 +12,18 @@ from wespeaker.models.transformer.bn import BatchNorm1d
 import wespeaker.models.pooling_layers as pooling_layers
 
 class Conformer(torch.nn.Module):
-    def __init__(self, n_mels=80, num_blocks=6, output_size=256, embedding_dim=192, input_layer="conv2d2", 
+    def __init__(self, feat_dim=80, embed_dim=192, pooling_func='ASTP', 
+            num_blocks=6, output_size=256, input_layer="conv2d2", 
             pos_enc_layer_type="rel_pos"):
 
         super(Conformer, self).__init__()
-        print("input_layer: {}".format(input_layer))
-        print("pos_enc_layer_type: {}".format(pos_enc_layer_type))
-        self.conformer = ConformerEncoder(input_size=n_mels, num_blocks=num_blocks, 
+        self.conformer = ConformerEncoder(input_size=feat_dim, num_blocks=num_blocks, 
                 output_size=output_size, input_layer=input_layer, pos_enc_layer_type=pos_enc_layer_type)
+        self.n_stats = 1 if pooling_func == 'TAP' or pooling_func == "TSDP" else 2
         self.pooling = pooling_layers.ASTP(in_dim=output_size*num_blocks)
-        self.bn = BatchNorm1d(input_size=output_size*num_blocks*2)
-        self.fc = torch.nn.Linear(output_size*num_blocks*2, embedding_dim)
+        self.pooling = getattr(pooling_layers, pooling_func)(in_dim=output_size*num_blocks)
+        self.bn = BatchNorm1d(input_size=output_size*num_blocks*self.n_stats)
+        self.fc = torch.nn.Linear(output_size*num_blocks*self.n_stats, embed_dim)
     
     def forward(self, feat):
         lens = torch.ones(feat.shape[0]).to(feat.device)
@@ -31,13 +36,16 @@ class Conformer(torch.nn.Module):
         x = x.squeeze(1)
         return x
 
-def MFA_Conformer6(feat_dim, embed_dim, pooling_func='TSTP'):
-    model = Conformer()
+def MFA_Conformer(feat_dim=80, embed_dim=192, pooling_func='ASTP', 
+        num_blocks=6, output_size=256, input_layer="conv2d2", 
+        pos_enc_layer_type="rel_pos"):
+    model = Conformer(feat_dim, embed_dim, pooling_func, 
+            num_blocks, output_size, input_layer, pos_enc_layer_type)
     return model
 
  
 if __name__ == '__main__':
-    model = MFA_Conformer6(feat_dim=80, embed_dim=512, pooling_func='TSTP')
+    model = MFA_Conformer(feat_dim=80, embed_dim=192, pooling_func='TSTP')
     y = model(torch.rand(2, 200, 80))
     print(y.shape)
 
